096c62c8cb770186f42ecd0c2f6fe84e19bf5ef7
Anita Vero <anitaveroe@gmail.com>
2022-07-20 12:20:23 +0000

    Allow list of model names in config

--- model_eval/run_zeroshot_on_jigsaw.py
+++ model_eval/run_zeroshot_on_jigsaw.py
@@ -13,33 +13,39 @@ def test_zeroshot(config, data_path=None, device="cuda:0", eval=False, s3_dir=No
 
     prompt_pattern = "This text is about {}"
 
-    files = run_zeroshot(
-        data_path,
-        candidate_labels=config["dataset"]["args"]["classes"],
-        model_name=config["arch"]["args"]["model_name"],
-        embeddings_file=None,
-        prompt_embeddings_file=None,
-        batch_size=config["batch_size"],
-        device=device,
-        prompt_pattern=prompt_pattern,
-        save_to="",
-        save_embeddings_to="pickle",
-        overwrite=True,
-        id_column="id",
-        text_column="comment_text",
-    )
+    model_names = config["arch"]["args"]["model_name"]
+    if isinstance(model_names, str):
+        model_names = [model_names]
 
-    print("Evaluate")
-    if eval:
-        files["metrics"] = eval_predictions.save_metrics(files["results"], config)
+    for model_name in model_names:
+        print(model_name)
+        files = run_zeroshot(
+            data_path,
+            candidate_labels=config["dataset"]["args"]["classes"],
+            model_name=model_name,
+            embeddings_file=None,
+            prompt_embeddings_file=None,
+            batch_size=config["batch_size"],
+            device=device,
+            prompt_pattern=prompt_pattern,
+            save_to="",
+            save_embeddings_to="pickle",
+            overwrite=True,
+            id_column="id",
+            text_column="comment_text",
+        )
 
-    if s3_dir is not None:
-        print("Copying to S3")
-        os.system(f'aws s3 cp {files["results"]} {os.path.join(s3_dir, "results/")}')
+        print("Evaluate")
         if eval:
-            os.system(f'aws s3 cp {files["metrics"]} {os.path.join(s3_dir, "results/")}')
-        os.system(f'aws s3 cp {files["embeddings"]} {os.path.join(s3_dir, "embeddings/")}')
-        os.system(f'aws s3 cp {files["prompt_embeddings"]} {os.path.join(s3_dir, "embeddings/")}')
+            files["metrics"] = eval_predictions.save_metrics(files["results"], config)
+
+        if s3_dir is not None:
+            print("Copying to S3")
+            os.system(f'aws s3 cp {files["results"]} {os.path.join(s3_dir, "results/")}')
+            if eval:
+                os.system(f'aws s3 cp {files["metrics"]} {os.path.join(s3_dir, "results/")}')
+            os.system(f'aws s3 cp {files["embeddings"]} {os.path.join(s3_dir, "embeddings/")}')
+            os.system(f'aws s3 cp {files["prompt_embeddings"]} {os.path.join(s3_dir, "embeddings/")}')
 
 
 if __name__ == "__main__":
