1e87d2db1b2c446beb2025e67f197eccc120b124
Anita Vero <anitaveroe@gmail.com>
2022-08-15 17:28:46 +0000

    Sweep code in funetuning module

--- scripts/finetune_embeddings_on_jigsaw.py
+++ scripts/finetune_embeddings_on_jigsaw.py
@@ -1,10 +1,13 @@
 import argparse
 import json
 import os
+from inspect import getfullargspec
 
 import numpy as np
 
 import pandas as pd
+import wandb
+import yaml
 from model_eval.eval_predictions import evaluate
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.multioutput import MultiOutputClassifier
@@ -15,7 +18,7 @@ from utils import load_embeddings
 from scripts.run_classifiers import run_zeroshot
 
 
-def finetune(config, device="cuda:0", s3_dir=None):
+def finetune(config, sweep_config=None, action="train", device="cuda:0", s3_dir=None):
     train_path = config["dataset"]["args"]["train_csv_file"]
     test_labels_path = config["dataset"]["args"]["test_labels_csv_file"]
     test_path = config["dataset"]["args"]["test_csv_file"]
@@ -86,23 +89,66 @@ def finetune(config, device="cuda:0", s3_dir=None):
         y_test_m = y_test[mask, :]
         test_embeddings_m = test_embeddings[mask, :]
 
-        print("Train")
-        if classifier_name == "mlp":
-            clf = MLPClassifier(random_state=1, max_iter=500, verbose=True)
-        elif classifier_name == "forest":
-            clf = RandomForestClassifier(random_state=1, n_jobs=7, verbose=True)
+        if action == "train":
+            train(
+                classifier_name,
+                train_embeddings,
+                y_train,
+                test_embeddings_m,
+                y_test_m,
+                results_dir,
+                model_name,
+                s3_dir,
+                files,
+            )
+        if action == "sweep":
+            sweep(
+                sweep_config,
+                classifier_name,
+                train_embeddings,
+                y_train,
+                test_embeddings_m,
+                y_test_m,
+                results_dir,
+                model_name,
+            )
 
-        mo_clf = MultiOutputClassifier(estimator=clf).fit(train_embeddings, y_train)
 
-        def predict_prob(embs):
-            preds = mo_clf.predict_proba(embs)
-            return np.column_stack([p[:, 1] for p in preds])
+def train(
+    classifier_name,
+    train_embeddings,
+    y_train,
+    test_embeddings_m,
+    y_test_m,
+    results_dir,
+    model_name,
+    param_config,
+    s3_dir=None,
+    embedding_files=None,
+    save_metrics=True,
+):
+    print("Train")
 
-        print("Predict")
-        scores = predict_prob(test_embeddings_m)
-        metrics = evaluate(scores, y_test_m)
-        print("Mean Acc:", clf.score(test_embeddings_m, y_test_m))
-        print(metrics)
+    def params(model):
+        {p: val for p, val in param_config.items() if p in getfullargspec(model).args}
+
+    if classifier_name == "mlp":
+        clf = MLPClassifier(verbose=True, **params(MLPClassifier))
+    elif classifier_name == "forest":
+        clf = RandomForestClassifier(n_jobs=7, verbose=True, **params(RandomForestClassifier))
+
+    mo_clf = MultiOutputClassifier(estimator=clf).fit(train_embeddings, y_train)
+
+    def predict_prob(embs):
+        preds = mo_clf.predict_proba(embs)
+        return np.column_stack([p[:, 1] for p in preds])
+
+    print("Predict")
+    scores = predict_prob(test_embeddings_m)
+    metrics = evaluate(scores, y_test_m)
+    print("Mean Acc:", clf.score(test_embeddings_m, y_test_m))
+    print(metrics)
+    if save_metrics:
         metrics_file = os.path.join(results_dir, f"metrics_finetune_{model_name}_{classifier_name}_multioutput.json")
         with open(metrics_file, "w") as f:
             json.dump(metrics, f)
@@ -110,8 +156,37 @@ def finetune(config, device="cuda:0", s3_dir=None):
         if s3_dir is not None:
             print("Copying to S3")
             os.system(f'aws s3 cp {metrics_file} {os.path.join(s3_dir, "results/")}')
-            for file in files:
+            for file in embedding_files:
                 os.system(f'aws s3 cp {file["embeddings"]} {os.path.join(s3_dir, "embeddings/")}')
+
+    return metrics
+
+
+def sweep(
+    sweep_config, classifier_name, train_embeddings, y_train, test_embeddings_m, y_test_m, results_dir, model_name
+):
+    with open(sweep_config) as stream:
+        sweep_config = yaml.safe_load(stream)
+    sweep_id = wandb.sweep(sweep_config)
+
+    def run_train():
+        wandb.init()  # required to have access to `wandb.config`
+        wb_config = wandb.config
+        metrics = train(
+            classifier_name,
+            train_embeddings,
+            y_train,
+            test_embeddings_m,
+            y_test_m,
+            results_dir,
+            model_name,
+            wb_config,
+            save_metrics=False,
+        )
+        wandb.log({"mean_auc": metrics["mean_auc"]})
+
+    count = 10  # number of runs to execute
+    wandb.agent(sweep_id, function=run_train, count=count)
 
 
 if __name__ == "__main__":
@@ -122,6 +197,21 @@ if __name__ == "__main__":
         default=None,
         type=str,
         help="config file path (default: None)",
+    )
+    parser.add_argument(
+        "-a",
+        "--action",
+        default="train",
+        type=str,
+        choices=["train", "sweep"],
+        help="Train models or sweep hyperparameters (default: train)",
+    )
+    parser.add_argument(
+        "-sc",
+        "--sweep_config",
+        default=None,
+        type=str,
+        help="Sweep config file path (default: None)",
     )
     parser.add_argument(
         "-d",
@@ -143,4 +233,4 @@ if __name__ == "__main__":
     if args.device is not None:
         config["gpus"] = args.device
 
-    results = finetune(config, args.device, args.s3_dir)
+    results = finetune(config, args.sweep_config, args.action, args.device, args.s3_dir)
