1183fad4e6532ad45e2f51e539dd90c1b9b94bda
Anita Vero <anitaveroe@gmail.com>
2022-09-26 12:28:57 +0000

    File pattern param in convert_pickles

--- scripts/convert_pickles.py
+++ scripts/convert_pickles.py
@@ -3,7 +3,9 @@ import argparse
 from utils import convert_pkls_embeddings
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert a pickle series file or a directory of pickle series files to webdataset format.")
+    parser = argparse.ArgumentParser(
+        description="Convert a pickle series file or a directory of pickle series files to webdataset format."
+    )
     parser.add_argument(
         "data_path",
         type=str,
@@ -12,9 +14,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "--to",
         type=str,
-        choices=['pickle', 'webdataset'],
-        default='pickle',
+        choices=["pickle", "webdataset"],
+        default="pickle",
         help="Format to convert to: 'pickle' or 'webdataset' (default: pickle)",
     )
+    parser.add_argument(
+        "-p",
+        "--file_pattern",
+        required=True,
+        type=str,
+        help="Pattern for files to copy",
+    )
     args = parser.parse_args()
-    convert_pkls_embeddings(args.data_path, args.to)
+    convert_pkls_embeddings(args.data_path, args.to, file_pattern=args.file_pattern)
